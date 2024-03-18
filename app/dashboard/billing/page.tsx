b505d6b9c957d32eee3f5c622cc3b67225c583bd
mustafa-motiwala07 <mustu.motiwala03@gmail.com>
2024-03-18 22:03:18 +0530

    Dashboard Index initialization

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -62,8 +62,16 @@ export default async function BillingPage() {
           });
       
           return redirect(subscriptionUrl);
+    }
 
-        return redirect(subscriptionUrl);
+    if(data?.status === 'active') {
+        <div className="grid items-start gap-8">
+            <div className="flex items-center justify-between px-2">
+                <div className="grid gap-1">
+                    
+                </div>
+            </div>
+        </div>
     }
 
     return(
